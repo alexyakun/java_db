4c516bbb6d6292c27af7840affe6b4f5470c151a
User <alexyakun@gmail.com>
2023-04-26 10:45:07 +0300

    Empty project

---
+++ src/main/java/ru/mpei/java_db/JavaDbApplication.java
@@ -0,0 +1,13 @@
+package ru.mpei.java_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mpei/java_db/JavaDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mpei.java_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
